ece54e8c7ba110b89bf88ad8fedb3954e895b147
Jordi <jordi8101@gmail.com>
2023-01-01 20:10:06 -0800

    Implement get posts query

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import gql from "graphql-tag";
+import { useQuery } from "@vue/apollo-composable";
+
+const POSTS_QUERY = gql`
+  query getAllPosts {
+    getAllPosts {
+      id
+      title
+      content
+      author {
+        id
+        name
+      }
+    }
+  }
+`;
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    const { result, loading, error } = useQuery(POSTS_QUERY);
+    return {
+      result,
+      loading,
+      error,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div id="app">
+    <h1>Posts</h1>
+    <div v-if="loading">Loading...</div>
+    <div v-if="error">Error: {{ error }}</div>
+    <div v-if="result">
+      <div v-for="post in result.getAllPosts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.content }}</p>
+        <p>Author: {{ post.author.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: 'http://localhost:5001/api',
+})
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
+
+app.mount('#app');
